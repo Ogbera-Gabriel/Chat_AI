4482453e3c0880aa6c342726dfb926e5d1a78237
Ogbera-Gabriel <ogberagabriel234@gmail.com>
2023-08-05 15:36:47 +0100

    New-Update

--- server/server.js
+++ server/server.js
@@ -1,13 +1,11 @@
-import  express  from "express";
+import express from "express";
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 import { Configuration, OpenAIApi } from "openai";
 
 dotenv.config();
 
-
-
-const configuration = new Configuration ({
+const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
 
@@ -17,34 +15,49 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Create a simple async queue to process API requests sequentially
+const requestQueue = [];
+let isProcessing = false;
 
-app.get('/', async (req, res) => {
-    res.status(200).send({
-        message: 'Hello from Codex'
-    })
-});
+const processRequestQueue = async () => {
+    if (requestQueue.length === 0 || isProcessing) return;
+    isProcessing = true;
 
-app.post('/', async (req, res) => {
-    try{
-      const prompt = req.body.prompt;
+    const { req, res } = requestQueue.shift();
 
-      const response = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: `${prompt}`,
-        temperature: 0,
-        max_tokens: 3000,
-        top_p: 1,
-        frequency_penalty: 0.5,
-        presence_penalty: 0,
-      })
+    try {
+        const prompt = req.body.prompt;
 
-      res.status(200).send({
-        bot: response.data.choices[0].text
-      })
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `${prompt}`,
+            temperature: 0,
+            max_tokens: 3000,
+            top_p: 1,
+            frequency_penalty: 0.5,
+            presence_penalty: 0,
+        });
+
+        res.status(200).send({
+            bot: response.data.choices[0].text
+        });
     } catch (error) {
         console.log(error);
-        res.status(500).send ({ error })
+        res.status(500).send({ error });
+    } finally {
+        isProcessing = false;
+        processRequestQueue(); // Process the next request in the queue, if any
     }
-})
+};
 
-app.listen(5000, ()=> console.log('Server is running on port http://localhost:5000'));
+app.post('/', async (req, res) => {
+    try {
+        requestQueue.push({ req, res });
+        processRequestQueue();
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ error });
+    }
+});
+
+app.listen(5000, () => console.log('Server is running on port http://localhost:5000'));
